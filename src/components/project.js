a8d382f7d37be8fc5fd19963c2268f5be799ce59
Kevin Darcy <neoglitch822@yahoo.com>
2020-02-12 14:33:27 -0500

    tweaked service worker for FireFox, temporary repair

--- src/components/project.js
+++ src/components/project.js
@@ -10,7 +10,7 @@ class Project extends Component {
             <img src={this.props.imageSrc} alt={this.props.title}></img>
             </a>
             <h1>{this.props.title}</h1>
-            <span>{this.props.service}</span>
+            <p>{this.props.service}</p>
         </div>);
     }
 }
