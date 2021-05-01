2a97ef264b9ecd3a0dc669610344d9d282f5e668
Kevin Darcy <neoglitch822@yahoo.com>
2021-05-01 16:42:16 -0400

    updated stack

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,7 +21,7 @@ class Contact extends Component {
                 </ul>
         </div>
 
-            <span className='footer'>Created by Kevin Michael | 2020 </span>
+            <span className='footer'>Created by Kevin Michael | 2021 </span>
         </div>);
     }
 }
